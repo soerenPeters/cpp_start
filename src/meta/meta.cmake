eb7d81bf361c17104d7b4937cb0e2fcdfe932d90
Soeren Peters <github@soerenpeters.com>
2020-11-24 14:57:33 +0100

    Rename projectInfo path.

--- src/meta/meta.cmake
+++ src/meta/meta.cmake
@@ -1,9 +1,8 @@
-set(buildInfoPath ${CMAKE_BINARY_DIR}/projectInfo)
-set(buildInfoFile meta.cpp)
-set(buildInfoInput ${CMAKE_CURRENT_LIST_DIR}/meta.in.cpp)
-set(buildInfoSourceFile ${buildInfoPath}/${buildInfoFile})
+set(buildMetaPath ${CMAKE_BINARY_DIR}/buildMetaPath)
+set(metaInput ${CMAKE_CURRENT_LIST_DIR}/meta.in.cpp)
+set(metaSourceFile ${buildMetaPath}/meta.cpp)
 
 include(${CMAKE_SOURCE_DIR}/CMake/git/GetGitRevisionDescription.cmake)
 get_git_head_revision(git_branch git_commit_hash)
 
-configure_file(${buildInfoInput} ${buildInfoSourceFile})
+configure_file(${metaInput} ${metaSourceFile})
