deb28111c6059d2a09a1ef85194c4317cabf7639
Soeren Peters <github@soerenpeters.com>
2020-12-17 17:17:02 +0100

    Adds include what you use option and ci

--- CMake/cpp_starter.cmake
+++ CMake/cpp_starter.cmake
@@ -10,6 +10,7 @@ option(CMAKE_VERBOSE_OUTPUT "Enable additional CMake output per target." ON)
 option(BUILD_COVERAGE "Add the --coverage compiler flag." OFF)
 option(BUILD_CLANG_TIDY "Enable clang-tidy checks." OFF)
 option(BUILD_CPPCHECK "Enable cppcheck." OFF)
+option(BUILD_INCLUDE_WHAT_YOU_USE "Enable Include what you use." OFF)
 
 option(BUILD_UNIT_TESTS "Add the unit test targets." ON)
 option(BUILD_WARNINGS_AS_ERRORS "Make all warnings into errors." OFF)
